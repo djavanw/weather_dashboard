4e5dc120d2dfdaff01c6b9e8135252a2a86757cd
Javan <71285290+djavanw@users.noreply.github.com>
2021-03-18 23:57:56 -0600

    Fixing previous history button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,14 @@ var ultraVEl = document.querySelector("#ultraV");
 
 var formSearchEl = document.querySelector("#formSearch");
 var citySearchEl = document.querySelector("#citySearch");
-
+var previousSearchItemsBtnEl = document.querySelector(".previousSearchItemsBtn");
 var searchBtnEl = document.querySelector("#searchBtn");
+var historyItemsEl = $("#historyItems");
+var previousSearchesBox = $("#previousSearches");
+var iconOneEl = document.querySelector("#iconOne"); 
 
-var iconOneEl = document.querySelector("#iconOne");         
+var storageCity = [];                                           //For city names in local storage
+
 // var iconTwoEl = document.querySelector("#iconTwo");          //These are not being used
 // var iconThreeEl = document.querySelector("#iconThree");
 // var iconFourEl = document.querySelector("#iconFour");
@@ -20,27 +24,11 @@ var iconBack  = "@2x.png";
 var apiFront = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apiBack = "&appid=" + yourApiKey + "&units=imperial";
 
-// var curdat = new Date(null); ;                               //This will convert seconds to date/time
-// curdat.setTime(1615785250 * 1000);
-// alert(curdat.toLocaleString());
-
-//http://openweathermap.org/img/wn/10d@2x.png                   //This is the link for the icons
-
 var apiForecastFront = "https://api.openweathermap.org/data/2.5/forecast?q=";
 
-
-//var uviLat;
-//var uviLon;
 var oneCallApi;
 var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=";
 
-
-
-// document.addEventListener('DOMContentLoaded', function() {        //Modal
-//     var elems = document.querySelectorAll('.modal');
-//     var options ="opacity";
-//     var instances = M.Modal.init(elems, options);
-//   });
 
 function searching(event) {                                             //This function is the current day forecast.
     event.preventDefault();
@@ -48,6 +36,15 @@ function searching(event) {                                             //This f
     var userCitySearch = citySearchEl.value.trim();
     var weatherAPI = apiFront + userCitySearch + apiBack;
     
+    var userInputTrimmed = $('<button class="previousSearchItemsBtn" type="button">');
+    userInputTrimmed.click(function(event) {
+        event.preventDefault();
+    });
+    storageCity.push(userCitySearch);
+    localStorage.setItem("storageCity", JSON.stringify(storageCity));
+    userInputTrimmed.text(userCitySearch);
+    previousSearchesBox.append(userInputTrimmed);
+    console.log(userInputTrimmed);
     console.log("You are in the searching function");
     fetch(weatherAPI) 
     .then(function(response) {
@@ -59,31 +56,28 @@ function searching(event) {                                             //This f
         console.log(uviLat);
         console.log(uviLon);
         var daIcon = `${data.weather[0].icon}`; 
-        iconOneEl.src = iconFront + daIcon + iconBack;
+        // iconOneEl.src = iconFront + daIcon + iconBack;
         console.log("You are in the searching function" + data);
         dailyForecastEl.innerHTML = 
         `
         <ul>
-        <li> Date..:  ${moment(data.dt,"X").format("MM/DD/YYYY")} </li>
-        <li> Name:  ${data.name} </li>
-        <li> Description:  ${data.weather[0].icon}</li>
+        <li> ${data.name} </li>
+        <li> <span class="mainDate"> ${moment(data.dt,"X").format("MM/DD/YYYY")} </span></li>
+        <li> <img src= ${iconFront + daIcon + iconBack}> </li>
         <li> Temp:  ${data.main.temp} &#8457 </li>
         <li> Humidity:  ${data.main.humidity}% </li>
         <li> Wind Speed:  ${data.wind.speed} MPH </li>
-        <li> UV Index:  ${data.main.temp} </li>
-        <li> Weather:  ${data.weather[0].main} </li>
         </ul>
         `
-        // var iconOneEl = document.querySelector('#iconOne');
-        // var iconFront = "http://openweathermap.org/img/wn/";
-        // var iconBack  = "@2x.png";
         ultraVioletIndex(uviLat, uviLon);
               
     })
-
     
-    
+  
 }
+
+
+
 
 function forecasting(event) {                                                   //This function starts the 5 day forecast.
     event.preventDefault();
@@ -97,84 +91,63 @@ function forecasting(event) {                                                   
         return response.json()
     })
     .then(function(data) {
-        console.log("You are in the 5 day Forecast" + data);
+        console.log(data);
         var daIcon2 = `${data.list[2].weather[0].icon}`;                        //This block of code will gather the icon code numbers from data 
-        // iconTwoEl.src = iconFront + daIcon2 + iconBack;                         //and get the weather icons from the website.
-
-        var daIcon3 = `${data.list[10].weather[0].icon}`; 
-        // iconThreeEl.src = iconFront + daIcon3 + iconBack;
-
+        var daIcon3 = `${data.list[10].weather[0].icon}`;                      //and get the weather icons from the website.
         var daIcon4 = `${data.list[18].weather[0].icon}`; 
-        //iconFourEl.src = iconFront + daIcon4 + iconBack;
-
         var daIcon5 = `${data.list[26].weather[0].icon}`; 
-        //iconFiveEl.src = iconFront + daIcon5 + iconBack;
-
         var daIcon6 = `${data.list[34].weather[0].icon}`; 
-        //iconSixEl.src = iconFront + daIcon6 + iconBack;
-
-   
+           
         document.querySelector("#two").innerHTML =                              //Data pull for day one of five day. 12noon
         `                                                                           
         <ul>
-        <li> date:  ${moment(data.list[2].dt,"X").format("MM/DD/YYYY")} </li>
+        <li>${moment(data.list[2].dt,"X").format("MM/DD/YYYY")}</li>
         <li> <img src= ${iconFront + daIcon2 + iconBack}> </li>
-        <li> Description:  ${data.list[2].weather[0].icon} </li>
         <li> Temp:  ${data.list[2].main.temp} &#8457 </li>
         <li> Humidity:  ${data.list[2].main.humidity}% </li>
         </ul>
         `
-           
-
         document.querySelector("#three").innerHTML =                            //Data pull for day two of five day. 12noon
         `
         <ul>
-        <li> date:  ${moment(data.list[10].dt,"X").format("MM/DD/YYYY")} </li>
+        <li> ${moment(data.list[10].dt,"X").format("MM/DD/YYYY")} </li>
         <li> <img src= ${iconFront + daIcon3 + iconBack}> </li>
-        <li> Description:  ${data.list[10].weather[0].icon} </li>
         <li> Temp:  ${data.list[10].main.temp} &#8457 </li>
         <li> Humidity:  ${data.list[10].main.humidity}% </li>
         </ul>
         `
-
         document.querySelector("#four").innerHTML =                            //Data pull for day three of five day. 12noon
         `
         <ul>
-        <li> date:  ${moment(data.list[18].dt, "X").format("MM/DD/YYYY")} </li>
+        <li> ${moment(data.list[18].dt, "X").format("MM/DD/YYYY")} </li>
         <li> <img src= ${iconFront + daIcon4 + iconBack}> </li>
-        <li> Description:  ${data.list[18].weather[0].icon} </li>
         <li> Temp:  ${data.list[18].main.temp} &#8457 </li>
         <li> Humidity:  ${data.list[18].main.humidity}% </li>
         </ul>
         `
-
         document.querySelector("#five").innerHTML =                             //Data pull for day four of five day. 12noon
         `
         <ul>
-        <li> date:  ${moment(data.list[26].dt, "X").format("MM/DD/YYYY")} </li>
+        <li> ${moment(data.list[26].dt, "X").format("MM/DD/YYYY")} </li>        
         <li> <img src= ${iconFront + daIcon5 + iconBack}> </li>
-        <li> Description:  ${data.list[26].weather[0].icon} </li>
         <li> Temp:  ${data.list[26].main.temp} &#8457 </li>
         <li> Humidity:  ${data.list[26].main.humidity}% </li>
         </ul>
         `
-
         document.querySelector("#six").innerHTML =                               //Data pull for day five of five day.
         `
         <ul>
         <li> ${moment(data.list[34].dt, "X").format("MM/DD/YYYY")} </li>
         <li> <img src= ${iconFront + daIcon6 + iconBack}> </li>
-        <li> Description:  ${data.list[34].weather[0].icon} </li>
         <li> Temp:  ${data.list[34].main.temp} &#8457 </li>
         <li> Humidity:  ${data.list[34].main.humidity}% </li>
         </ul>
         `
         
     })
-
-
-  
+ 
 }
+
 
 
 function ultraVioletIndex(uviLat, uviLon) {  
@@ -195,7 +168,8 @@ function ultraVioletIndex(uviLat, uviLon) {
         console.log("You are in the UVI");   
         console.log(data);
         console.log(data.current.uvi);
-        ultraVEl.innerHTML = `
+        ultraVEl.innerHTML =
+         `
         <ul>
         <li>UV Index:  <span class="uviData">${data.current.uvi}</span></li>
         <ul>
@@ -208,20 +182,39 @@ function ultraVioletIndex(uviLat, uviLon) {
         } else {
             document.querySelector(".uviData").style.background = "yellow"; 
         };
-            
-
-     
-
-
-
+  
     })
 }
 
-//ultraVEl.style.background 
-formSearchEl.addEventListener("submit", forecasting);
-formSearchEl.addEventListener("submit", searching);
-//formSearchEl.addEventListener("submit", ultraVioletIndex);
+function pullStorageCity () {
+    if(localStorage.getItem("storageCity")) {
+        storageCity = JSON.parse(localStorage.getItem("storageCity"));
+        for(let k = 0; k < storageCity.length; k++) {
+            var userInputTrimmed = $('<button class="previousSearchItemsBtn" type="button">');
+            // userInputTrimmed.click(function(event) {
+            //     event.preventDefault();
+            // })
+            userInputTrimmed.text(storageCity[k]);
+            userInputTrimmed.on("click", searching);
+            previousSearchesBox.append(userInputTrimmed);   
+        }
+    }
+   
+}
 
+
+pullStorageCity(); 
+//ultraVEl.style.background
+// previousSearchItemsBtnEl.addEventListener('click', )
+
+
+searchBtnEl.addEventListener("click", forecasting);
+searchBtnEl.addEventListener("click", searching);
+// previousSearchItemsBtnEl.addEventListener("click", searching);
+
+
+
+//formSearchEl.addEventListener("submit", ultraVioletIndex);
 
 // fetch("https://api.openweathermap.org/data/2.5/weather?q=El Paso&appid=639e73227e908c5498c3c5be9b183ef8&units=imperial")
 // .then(function(response){
